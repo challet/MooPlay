dae782b49acef6f79bd97d2fe2659e3743d78dd7
challet <clement.hallet@af83.com>
2010-04-02 21:19:45 +0200

    rename ButtonBase into BaseButton

---
+++ Source/Control/BaseButton.js
@@ -0,0 +1,49 @@
+
+
+MooPlay.Control.BaseButton = new Class({
+
+    Implements: [Options],
+    
+    options: {
+        over_state_class: 'over',
+        click_state_class: 'clicked'
+    },
+    
+    
+    initialize: function(element, video, options) {
+        
+        this.setOptions(options);
+        
+        this.element = $(element);
+        this.video = $(video);
+
+        this.element.addEvents({
+            
+            'mouseenter': function(event) {
+                event.preventDefault();
+                this.element.addClass(this.options.over_state_class);
+            }.bind(this),
+            
+            'mouseleave': function(event) {
+                event.preventDefault();
+                this.element.removeClass(this.options.over_state_class);
+            }.bind(this),
+        
+            'mousedown': function(event) {
+                event.preventDefault();
+                this.element.addClass(this.options.click_state_class);
+            }.bind(this),
+        
+            'mouseup': function(event) {
+                event.preventDefault();
+                this.element.removeClass(this.options.click_state_class);
+            }.bind(this)
+            
+        });
+        
+        this.specificInitialize();
+        
+    }
+    
+
+});
