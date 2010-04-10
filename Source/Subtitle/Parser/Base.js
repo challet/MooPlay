e15d7b881236f04baad331f525dbc8e98041d8d7
challet <clement.hallet@af83.com>
2010-04-10 17:03:25 +0200

    experiments about multi format loading

--- Source/Subtitle/Parser/Base.js
+++ Source/Subtitle/Parser/Base.js
@@ -22,24 +22,14 @@ MooPlay.Subtitle.Parser.Base = new Class({
     
     Implements: [Options],
     
-    initialize: function(options) {
+    initialize: function(data, options) {
         this.setOptions(options);
-        this.load();            
-    },
-
-    load: function() {
-
-        var request = new Request({
-            url: this.options.url,
-            method: 'get',
-            onSuccess: function(data) {
-                this.run(this.parse(data));
-            }.bind(this)
-        });
-        request.send({});
+        this.hash(
+            this.parse(data)
+        );
     },
     
-    run: function (subs) {
+    hash: function(subs) {
         
         var abs_start = Infinity;
         var abs_end = 0;
@@ -47,20 +37,14 @@ MooPlay.Subtitle.Parser.Base = new Class({
             abs_start = Math.min(abs_start, sub.start);
             abs_end = Math.max(abs_end, sub.end);
         });
-        this.hash_root = this.hash(abs_start, abs_end, subs);
-        this.options.onComplete(this.hash_root);
         
-    },
-    
-    hash: function(abs_start, abs_end, subs) {
-        
-        var hash_root = new MooPlay.Subtitle.Tree(abs_start, abs_end);
+        this.hash_root = new MooPlay.Subtitle.Tree(abs_start, abs_end);
 
         subs.each(function(sub) {
-            hash_root.addSub(sub);
-        });
+            this.hash_root.addSub(sub);
+        }.bind(this));
         
-        return hash_root;
+        this.options.onComplete(this.hash_root);
     }
 
 });
