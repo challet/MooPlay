f173698eb5d6fd8e1e897e7360d597189de51925
Clément Hallet <clement.hallet@af83.com>
2010-07-05 01:42:43 +0200

    script merger

--- sourcemerge.sh
+++ sourcemerge.sh
@@ -6,7 +6,7 @@ dest="Assets/mooplay.$version.js"
 `touch $dest`
 `echo '' > $dest`
 
-for file in `find Source -name *.js`
+for file in `cat package.yml | sed -n -e 's/^files: \[\([a-zA-Z0-9/., ]*\)\]$/\1/p' | awk -F ", " '{for(i=1;i<=NF;i++) print $i }'`
 do
-    `cat $file >> $dest`
+    `cat Source/$file >> $dest`
 done
